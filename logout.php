3fcb9c07a66c463581e2a3208a65b0fad131337e
cuongluis16 <200062pf@yse-c.net>
2021-10-20 14:15:41 +0900

    終わり

--- logout.php
+++ logout.php
@@ -1,15 +1,14 @@
 <?php
 /* 
 【機能】
-	セッション情報を削除しログイン画面に遷移する。。
+	セッション情報を削除しログイン画面に遷移する
 */
 
 //①セッションを開始する。
 session_start();
 //②セッションを削除する。
-if(isset($_SESSION["user"])) {
-unset($_SESSION["user"]);
+unset($_SESSION["id"]);
+unset($_SESSION["name"]);
 //③ログイン画面へ遷移する。
-header("Location: login.php");
-}
+header("Location:login.php")
 ?>
